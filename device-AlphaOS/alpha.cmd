19b2e7fa8c903977812ad28a592f079f2e230392
2-2-gif <kjmcapitals1@gmail.com>
2025-07-30 22:20:23 +0300

    Update alpha.cmd

--- device-AlphaOS/alpha.cmd
+++ device-AlphaOS/alpha.cmd
@@ -1,1 +1,1 @@
-cd C:/Users/mm && (echo alpha & type alpha.txt) > alpha_temp.txt move /Y alpha_temp.txt alpha.txt
+cd C:/Users/mm && (echo alpha & type alpha.txt) > alpha_temp.txt && move /Y alpha_temp.txt alpha.txt
